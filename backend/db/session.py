902ec3e7c5ccec43f952584f5992492b7b90c36e
Prabhat Ranjan <32527844+drive-deep@users.noreply.github.com>
2025-03-02 09:27:51 +0000

    fixed postgres db connection issue

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
+DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/orders_db"
+
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
